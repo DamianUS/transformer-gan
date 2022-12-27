8bc7b27eedd4d934a2cb68e0e1453eb932de78b6
DamianUS <damiancerero@us.es>
2022-12-27 21:31:22 +0100

    loss discriminator

--- trainer.py
+++ trainer.py
@@ -131,7 +131,7 @@ class StepByStep(object):
             loss_discriminator_real = self.loss_fn(pred_real, torch.ones_like(pred_real))
             loss_discriminator_fake = self.loss_fn(pred_fake, torch.zeros_like(pred_fake))
             loss_discriminator = (loss_discriminator_real + loss_discriminator_fake) * 0.5
-            if loss_discriminator_real > 0.15 and loss_discriminator_fake > 0.15:
+            if loss_discriminator > 0.15:
                 loss_discriminator.backward()
                 self.discriminator_optimizer.step()
             # Returns the loss
