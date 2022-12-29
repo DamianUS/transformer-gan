8f9313a34895a5a1f5487a8aa356635d9bab0048
DamianUS <damiancerero@us.es>
2022-12-29 22:54:35 +0100

    adam betas

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ def main(args):
     model = TransformerGAN(num_features=n_features, seq_len=seq_len, batch_size=batch_size, gen_num_layers=gen_num_layers, dis_num_layers=dis_num_layers, gen_hidden_dim=gen_hidden_dim, dis_hidden_dim=dis_hidden_dim, gen_narrow_attn_heads=gen_narrow_attn_heads, dis_narrow_attn_heads=dis_narrow_attn_heads, gen_dropout=gen_dropout, dis_dropout=dis_dropout, noise_length=100)
     model.to(args.device)
     loss = nn.BCEWithLogitsLoss()
-    generator_optimizer = optim.Adam(model.generator.parameters(), lr=gen_lr, betas=(0.5, 0.9))
-    discriminator_optimizer = optim.Adam(model.discriminator.parameters(), lr=dis_lr, betas=(0.5, 0.9))
+    generator_optimizer = optim.Adam(model.generator.parameters(), lr=gen_lr, betas=(0.5, 0.999))
+    discriminator_optimizer = optim.Adam(model.discriminator.parameters(), lr=dis_lr, betas=(0.5, 0.999))
     #generator_optimizer = optim.Adam(model.generator.parameters(), lr=gen_lr, betas=(0.5, 0.999))
     #discriminator_optimizer = optim.Adam(model.discriminator.parameters(), lr=dis_lr, betas=(0.5, 0.999))
 
