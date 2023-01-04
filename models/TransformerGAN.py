7e37114f32904edc5364803b10d6d7714999923b
DamianUS <damiancerero@us.es>
2023-01-04 20:30:24 +0100

    Removed gradient penalty, adding spectral normalization.

--- models/TransformerGAN.py
+++ models/TransformerGAN.py
@@ -14,8 +14,7 @@ class TransformerGAN(torch.nn.Module):
         self.discriminator = DiscriminatorTransformer(n_features=num_features, hidden_dim=dis_hidden_dim, seq_len=seq_len, narrow_attn_heads=dis_narrow_attn_heads, num_layers=dis_num_layers, dropout=dis_dropout)
         spectral_modules = [(name, torch.nn.utils.parametrizations.spectral_norm(module)) for name,module in self.discriminator.named_modules() if isinstance(module, torch.nn.Linear)]
         for name, spectral_module in spectral_modules:
-            print(name, spectral_module)
-            #self.discriminator._modules[name] = spectral_module
+            self.discriminator._modules[name] = spectral_module
 
     def forward(self, X, obj='discriminator'):
         #print([name for name, _ in self.discriminator.named_children()])
